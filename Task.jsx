4b168ac9cb5f4b3bde1e7a81d90cc5090c585504
Prawee Wongsa <prawee@hotmail.com>
2015-11-24 16:50:10 +0700

    add toggle checked()

--- Task.jsx
+++ Task.jsx
@@ -2,6 +2,11 @@ Task=React.createClass({
     propTypes:{
         task:React.PropTypes.object.isRequired
     },
+    toggleChecked(){
+        Tasks.update(this.props.task._id,{
+            $set:{checked:!this.props.task.checked}
+        });
+    },
     render(){
         return (
             <li>{this.props.task.text}</li>
