ae00ecf9bf1b9967c8b2ac36427296b5502be05d
Prawee Wongsa <prawee@hotmail.com>
2015-11-24 16:26:10 +0700

    update using data from collection instead fixed data.

--- App.jsx
+++ App.jsx
@@ -1,13 +1,23 @@
 App =React.createClass({
+    /*
     getTasks(){
         return [
             {_id:1,text:"This is taks 1"},
             {_id:2,text:"This is task 2"},
             {_id:3,text:"This is task 3"}
         ];
+    },*/
+    mixins:[ReactMeteorData],
+    getMeteorData(){
+        return {
+            tasks:Tasks.find({}).fetch()
+        }
     },
     renderTasks(){
-        return this.getTasks().map((task)=>{
+        /*return this.getTasks().map((task)=>{
+            return <Task key={task._id} task={task} />;
+        });*/
+        return this.data.tasks.map((task)=>{
             return <Task key={task._id} task={task} />;
         });
     },
