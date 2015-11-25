b01cfa5c6ead8f7fe52de5135165b3ee514b4bb3
Prawee Wongsa <prawee@hotmail.com>
2015-11-25 14:11:41 +0700

    configure accounts-ui to only require username

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -1,6 +1,11 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
+
+    Accounts.ui.config({
+        passwordSignupFields:'USERNAME_ONLY'
+    });
+
     Meteor.startup(function(){
         React.render(<App />,document.getElementById("render-target"));
     });
