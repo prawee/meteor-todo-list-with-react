1a4dd345cbf003d87a12e996ecda3b451f347965
Prawee Wongsa <prawee@hotmail.com>
2015-11-25 14:53:46 +0700

    add publish to server to client

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -34,7 +34,7 @@ Meteor.methods({
 });
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish("tasks",function(){
+      return Tasks.find();
   });
 }
